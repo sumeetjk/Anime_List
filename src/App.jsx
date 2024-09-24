bc17a37d6db8e03bafba98b2901d67a7a107a55a
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-24 22:51:57 +0530

    initialize the anime list react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Anime List App!!!</h1>
+      <hr />
+      <p>
+        Have no idea as of now what and how to make this app but will do
+        research and finish this app very soon, lets hope to finish this before
+        December 2024.
+      </p>
+    </div>
+  );
+}
